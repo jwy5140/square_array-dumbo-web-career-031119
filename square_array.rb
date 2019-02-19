f1115cb639b3a53950e6345a1652d8eab0561e52
Jeffrey Yu <jwy5140@gmail.com>
2019-02-19 23:57:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,18 +1,9 @@
 ## Originally requested solution
 
-# def square_array(array)
-#   n = 0 
-#   array.each do |num|
-#     array[n] = num**2 
-#     n += 1
-#   end
-# end
-
-## Preferred solution
-
 def square_array(array)
+  n = 0 
   array.each do |num|
-    array.at
+    array[n] = num**2 
     n += 1
   end
 end
