21e404c4da3e1ae794dd55337257087aa343ddf2
Jeffrey Yu <jwy5140@gmail.com>
2019-02-19 23:52:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,18 @@
+## Originally requested solution
+
+# def square_array(array)
+#   n = 0 
+#   array.each do |num|
+#     array[n] = num**2 
+#     n += 1
+#   end
+# end
+
+## Preferred solution
+
 def square_array(array)
-  n = 0 
   array.each do |num|
-    array[n] = num**2 
+    array.at
     n += 1
   end
 end
